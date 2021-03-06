344a75125d510a2dd90cbc581414ec8a266d963b
Maykon Mori <maykonmori@gmail.com>
2021-03-06 13:05:25 -0300

    Exercicios Go

---
+++ contas/ContaCorrente.go
@@ -0,0 +1,43 @@
+package contas
+
+import "banco/clientes"
+
+type ContaCorrente struct {
+	Titular                    clientes.Titular
+	NumeroAgencia, NumeroConta int
+	Saldo                      float64
+}
+
+func (c *ContaCorrente) Sacar(valorDoSaque float64) string {
+	podeSacar := valorDoSaque > 0 && valorDoSaque <= c.Saldo
+	if podeSacar {
+		c.Saldo -= valorDoSaque
+		return "Saque realizado com sucesso"
+	} else {
+		return "Saldo insuficiente"
+	}
+}
+
+func (c *ContaCorrente) Depositar(valorDeposito float64) (string, float64) {
+
+	if valorDeposito > 0 {
+		c.Saldo += valorDeposito
+		return "Deposito realizado com sucesso", c.Saldo
+	} else {
+		return "Valor do deposito menor que zero", c.Saldo
+	}
+}
+
+func (c *ContaCorrente) Transferir(valorDaTransferencia float64, contaDestino *ContaCorrente) bool {
+	if valorDaTransferencia < c.Saldo && valorDaTransferencia > 0 {
+		c.Saldo -= valorDaTransferencia
+		contaDestino.Depositar(valorDaTransferencia)
+		return true
+	} else {
+		return false
+	}
+}
+
+func (c *ContaCorrente) ObterSaldo() float64 {
+	return c.Saldo
+}
---
+++ contas/ContaPoupanca.go
@@ -0,0 +1,33 @@
+package contas
+
+import "banco/clientes"
+
+type ContaPoupanca struct {
+	Titular                              clientes.Titular
+	NumeroAgencia, NumeroConta, Operacao int
+	saldo                                float64
+}
+
+func (c *ContaPoupanca) Sacar(valorDoSaque float64) string {
+	podeSacar := valorDoSaque > 0 && valorDoSaque <= c.saldo
+	if podeSacar {
+		c.saldo -= valorDoSaque
+		return "Saque realizado com sucesso"
+	} else {
+		return "Saldo insuficiente"
+	}
+}
+
+func (c *ContaPoupanca) Depositar(valorDeposito float64) (string, float64) {
+
+	if valorDeposito > 0 {
+		c.saldo += valorDeposito
+		return "Deposito realizado com sucesso", c.saldo
+	} else {
+		return "Valor do deposito menor que zero", c.saldo
+	}
+}
+
+func (c *ContaPoupanca) ObterSaldo() float64 {
+	return c.saldo
+}
